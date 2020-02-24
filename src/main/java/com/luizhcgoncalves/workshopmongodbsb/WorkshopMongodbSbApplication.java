d1c9029de585a007df1cf6c1030304b057389268
Luiz Henrique Carneiro Gonçalves <goncalves.luizhc@gmail.com>
2020-02-24 12:22:05 -0300

    First commit - Project created

---
+++ src/main/java/com/luizhcgoncalves/workshopmongodbsb/WorkshopMongodbSbApplication.java
@@ -0,0 +1,13 @@
+package com.luizhcgoncalves.workshopmongodbsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongodbSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongodbSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizhcgoncalves/workshopmongodbsb/WorkshopMongodbSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizhcgoncalves.workshopmongodbsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongodbSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
