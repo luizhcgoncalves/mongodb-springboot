2597c22b0f45ddb4d393fe0d56c78779367339b3
Luiz Henrique Carneiro Gonçalves <goncalves.luizhc@gmail.com>
2020-02-24 15:09:00 -0300

    Consulta simples com @Query

--- src/main/java/com/luizhcgoncalves/workshopmongodbsb/repositories/PostRepository.java
+++ src/main/java/com/luizhcgoncalves/workshopmongodbsb/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.luizhcgoncalves.workshopmongodbsb.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.luizhcgoncalves.workshopmongodbsb.domain.Post;
@@ -11,4 +12,7 @@ import com.luizhcgoncalves.workshopmongodbsb.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>  {
 
 	List<Post> findByTitleContainingIgnoreCase(String txt);
+	
+	@Query("{ 'title': {$regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String txt);
 }
