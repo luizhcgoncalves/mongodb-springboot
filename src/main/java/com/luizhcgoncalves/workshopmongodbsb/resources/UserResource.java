2b5796bec7c4d4c8a31c510a5eacfefe9748463d
Luiz Henrique Carneiro Gonçalves <goncalves.luizhc@gmail.com>
2020-02-24 14:42:06 -0300

    Endpoint para retornar os posts de um usuários

--- src/main/java/com/luizhcgoncalves/workshopmongodbsb/resources/UserResource.java
+++ src/main/java/com/luizhcgoncalves/workshopmongodbsb/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.luizhcgoncalves.workshopmongodbsb.domain.Post;
 import com.luizhcgoncalves.workshopmongodbsb.domain.User;
 import com.luizhcgoncalves.workshopmongodbsb.dto.UserDTO;
 import com.luizhcgoncalves.workshopmongodbsb.services.UserService;
@@ -63,6 +64,12 @@ public class UserResource {
 		obj = service.update(obj);
 		
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
