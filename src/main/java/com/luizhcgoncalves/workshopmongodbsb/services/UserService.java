9caf3ae5969baf465d5411dedec9f894fc257912
Luiz Henrique Carneiro Gonçalves <goncalves.luizhc@gmail.com>
2020-02-24 13:52:18 -0300

    Inserção de usuário com POST

--- src/main/java/com/luizhcgoncalves/workshopmongodbsb/services/UserService.java
+++ src/main/java/com/luizhcgoncalves/workshopmongodbsb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luizhcgoncalves.workshopmongodbsb.domain.User;
+import com.luizhcgoncalves.workshopmongodbsb.dto.UserDTO;
 import com.luizhcgoncalves.workshopmongodbsb.repositories.UserRepository;
 import com.luizhcgoncalves.workshopmongodbsb.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
