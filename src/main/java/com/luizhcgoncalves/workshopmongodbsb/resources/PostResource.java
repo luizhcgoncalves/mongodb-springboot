8f8ad7f3bc95cfb528676c5d3cca1cd389c4ed51
Luiz Henrique Carneiro Gonçalves <goncalves.luizhc@gmail.com>
2020-02-24 15:04:35 -0300

    Consulta simples com query methods

--- src/main/java/com/luizhcgoncalves/workshopmongodbsb/resources/PostResource.java
+++ src/main/java/com/luizhcgoncalves/workshopmongodbsb/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.luizhcgoncalves.workshopmongodbsb.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luizhcgoncalves.workshopmongodbsb.domain.Post;
+import com.luizhcgoncalves.workshopmongodbsb.resources.util.URL;
 import com.luizhcgoncalves.workshopmongodbsb.services.PostService;
 
 @RestController
@@ -21,5 +25,12 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String txt) {
+		txt = URL.decodeParam(txt);
+		List<Post> list = service.findByTitle(txt);
+		return ResponseEntity.ok().body(list);
 	}
 }
